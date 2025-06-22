9c270362849d3ede22afe84935e8b76bf3da5c10
Your Name <your.email@example.com>
2025-06-22 07:54:07 +0530

    first commit

---
+++ javascript/demo.js
@@ -0,0 +1,18 @@
+// function abc()
+// {
+// console.log('hi')
+// }
+// abc()
+
+// function xyz()
+// {
+// console.log('hi')
+// }
+// xyz()
+
+// function phone()
+// {
+// console.log('this phone')
+// }
+// phone()
+
