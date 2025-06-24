ee7665bacd61eb58bbc0aebea74c1eba8563233c
Your Name <your.email@example.com>
2025-06-24 11:55:51 +0530

    function

--- javascript/demo.js
+++ javascript/demo.js
@@ -16,3 +16,26 @@
 // }
 // phone()
 
+
+// function myFunction(p1, p2) {
+//   return p1 * p2;
+
+// }
+// myFunction(3,5)
+// console.log(myFunction(3,5));
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(5, 10));
+
+
+var a = 10;
+var b = 20;
+
+function add()
+{
+    console.log(a+b)
+}
+
