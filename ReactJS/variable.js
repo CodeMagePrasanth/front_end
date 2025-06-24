593d1d2b2581fb4ec08af615c2de29318fb24074
Your Name <prasanthsenthilkumaran@gmail.com>
2025-06-24 12:28:49 +0530

    green box

---
+++ ReactJS/variable.js
@@ -0,0 +1,61 @@
+//var a = 10
+//let b = 20
+
+//console.log(a);
+
+/*{
+    var a = 10
+}
+console.log(a);
+*/
+
+/*{
+    let a = 10
+    console.log(a);
+}
+*/
+//console.log(a);
+
+//const  a = 10
+//console.log(a);
+
+// let a = 10
+// a = 20
+// console.log(a);
+
+// // variable exercise 1
+
+// var Price = 100;
+// var Product = 'Iphone17';
+// var Tax = 2;
+// console.log(Product);
+// var total = (Price + Tax)
+// console.log(total);
+
+
+// variable exercise 2
+
+
+// var fruitname = 'apple';
+// var count = 10;
+// var price = 100;
+// var total = (count * price);
+// console.log(total);
+
+
+
+// operators increment / decrement
+
+// let x = 5;
+// let b = ++x; // pre-increment: x is incerement to 6,and then assigned to y
+// console.log(x);
+// console.log(b);
+
+//typeof
+//console.log(typeof (true));
+
+
+var a = 20;
+var b = 30;
+console.log(a/b);
+
