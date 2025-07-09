e64bb6c7f84038a77c11c00a6e43aff5916ab725
Your Name <prasanthsenthilkumaran@gmail.com>
2025-07-09 08:58:07 +0530

    funtions

---
+++ javascript/function.js
@@ -0,0 +1,13 @@
+var factor = "kamal"
+var fplayers = "dhoni"
+var fmovies = "anbu"
+
+
+function Fav()
+{
+console.log("favorite Actor :"+factor)
+console.log("favorite Actor :"+fplayers)
+console.log("favorite Actor :"+fmovies)
+}
+
+Fav()
