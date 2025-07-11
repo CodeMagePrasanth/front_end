3913a7db5537bca142b27210f87fa80e34fa7121
Your Name <prasanthsenthilkumaran@gmail.com>
2025-07-11 10:09:43 +0530

    if

--- javascript/function.js
+++ javascript/function.js
@@ -1,13 +1,41 @@
-var factor = "kamal"
-var fplayers = "dhoni"
-var fmovies = "anbu"
+// var factor = "kamal"
+// var fplayers = "dhoni"
+// var fmovies = "anbu"
 
 
-function Fav()
-{
-console.log("favorite Actor :"+factor)
-console.log("favorite Actor :"+fplayers)
-console.log("favorite Actor :"+fmovies)
-}
+// function Fav()
+// {
+// console.log("favorite Actor :"+factor)
+// console.log("favorite Actor :"+fplayers)
+// console.log("favorite Actor :"+fmovies)
+// }
 
-Fav()
+// Fav()
+
+
+
+// parameters
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(19,5)
+
+// function area(l,b)
+// {
+//     var a = l*b
+//     console.log("area is :"+a)
+
+// }
+
+// area(20,66)
+
+
+//  return type
+
+// function myname()
+// {
+//     return "prasath"
+// }
+// var a = myname()
+// console.log(a)
---
+++ javascript/if.js
@@ -0,0 +1,26 @@
+// if(false)
+// {
+//     console.log("it's true")
+// }
+// else
+// {
+//    console.log("it's true")
+// } 
+
+//  logical AND &&
+// console.log(false && true) // true
+
+//  logical OR ||
+
+// console.log(false || true || false) // true
+
+//  logical NOT !
+// console.log(!true) //false
+
+if(red)
+{
+    console.log("stop")
+}
+else{
+    
+}
